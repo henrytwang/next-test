b966128f201c50dcf4b2e4c7705d408a0f61aede
Henry Wang <henry.t.wang@gmail.com>
2013-08-04 15:40:00 -0700

    update Gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+
+  attr_accessible :username, :email, :password, :password_confirmation
+
+  has_secure_password
+
+  validates :email, :uniqueness => { :case_sensitive => false }, :format => /\w+@\w+\.\w{2,4}/
+  validates :username, :uniqueness  => { :case_sensitive => false }, :length => {:minimum => 4}
+  validates :password, :length => {:minimum => 6}
+
+  validates_confirmation_of :password
+
 end
